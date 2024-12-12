3cd4bcdd338f3b6bf64b1ee02ce2fd79dad632dc
alt-Lexy <testpourledeveloppement@outlook.fr>
2024-12-12 16:23:48 +0100

    feat(web-app):  deduplicate code and use CRUD operation

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -16,6 +16,7 @@ namespace CRUDAppp.Controllers
          */
         public IActionResult Edit([FromRoute] int? id)
         {
+            ViewBag.Action = "edit";
             var categoryId = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
             return View(categoryId);
         }
@@ -23,7 +24,38 @@ namespace CRUDAppp.Controllers
         [HttpPost]
         public IActionResult Edit(Category category) 
         {
-            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            if (ModelState.IsValid) 
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+            
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromForm]Category category) 
+        {
+            if (ModelState.IsValid) 
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int categoryId) 
+        { 
+            CategoriesRepository.DeleteCategory(categoryId);
             return RedirectToAction(nameof(Index));
         }
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace CRUDAppp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppp.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
     }
 }
